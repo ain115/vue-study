57f51633e6f90d78f4c5ad3f8166079cba5e0828
ain115 <c00a@naver.com>
2019-01-01 16:59:04 +0900

    vue 게시판

---
+++ bootstrap-bts/src/App.vue
@@ -0,0 +1,27 @@
+26 lines (24 sloc)  409 Bytes
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-notice/src/components/Detail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div>{{data.writer}}</div>
+        <div>{{data.title}}</div>
+        <div>{{data.content}}</div>
+        <button @click="updateData">수정</button>
+        <button @click="deleteData">삭제</button>
+    </div>
+</template>
+<script>
+import data from '@/data'
+
+export default {
+    name: 'Detail',
+    data() {
+        //contentId는 각 글의 index값을 가져옴
+        const index = this.$route.params.contentId
+        return {
+            data: data[index],
+            index
+        }
+    },
+    methods: {
+        deleteData() {
+            data.splice(this.index, 1)
+            this.$router.push({
+                path: '/'
+            })
+        },
+        //글쓰기 폼으로 이동
+        updateData(index) {
+            this.$router.push({
+                name: 'Create',
+                params: {
+                    contentId: this.index
+                }
+            })
+        },
+    }
+}
+</script>
