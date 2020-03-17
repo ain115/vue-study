6c44d3277ce26088a875d9b2571fc1ab84c30595
ain11 <c00a@naver.com>
2020-03-17 17:18:19 +0900

    야구게임 프로젝트구조와 웹팩빌드

--- numBaseball/webpack.config.js
+++ numBaseball/webpack.config.js
@@ -1,15 +1,17 @@
+const path = require('path');//node에서 절대경로 작성을 쉽게 해주기위해 사용할 수 있도록 만들어둔 path메서드(모듈?)
+
 module.exports = {
     entry: {
-        app: './main.js',
+        app: path.join(__dirname, 'main.js'),
     },
     module: {
         rules: [{
-            
+
         }],
     },
     plugins: [],
     output: {
         filemane: 'app.js',
-        path: './dist',
+        path: path.join(__dirname, 'dist'),
     }
 };
