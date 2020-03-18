cdcc4d3f17ea0e6ac3bf0be009b1e53255a34127
ain11 <c00a@naver.com>
2020-03-18 12:26:59 +0900

    반응속도체크  "build": "webpack --watch" -> --watch를 넣으면 vue파일 수정될때마다 자동으로 build됨

---
+++ speed/ResponseCheck.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div id="screen"></div>
+    <div>
+      <div>평균시간: {{}}</div>
+      <button @:click="onReset">리셋</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+
+    },
+    methods: {
+      onReset() {
+        
+      }
+    }
+  };
+</script>
+
+<style>
+</style>
